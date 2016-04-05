0298d40263a05a00f6c4cc3dbdc8c1ef8a86ff4c
xavier de aguirre <xavier_deaguirre002@hotmail.com>
2016-04-05 11:26:15 +0200

    Test swipe

---
+++ app/src/androidTest/java/com/example/xavier/stageproject/CoursesTest.java
@@ -0,0 +1,84 @@
+package com.example.xavier.stageproject;
+
+import org.junit.Test;
+
+/**
+ * Created by xavier on 4/04/2016.
+ */
+public class CoursesTest {
+
+    @Test
+    public void testGetStudent_id() throws Exception {
+
+    }
+
+    @Test
+    public void testSetStudent_id() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDag() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDag() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPeriod() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPeriod() throws Exception {
+
+    }
+
+    @Test
+    public void testGetRoom() throws Exception {
+
+    }
+
+    @Test
+    public void testSetRoom() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDescription() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDescription() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFirst_name() throws Exception {
+
+    }
+
+    @Test
+    public void testSetFirst_name() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLaste_name() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLaste_name() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
--- app/src/androidTest/java/com/example/xavier/stageproject/MainActivityTest.java
+++ app/src/androidTest/java/com/example/xavier/stageproject/MainActivityTest.java
@@ -9,9 +9,18 @@ import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isChecked;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.IsNot.not;
 
 /**
  * Basic tests showcasing simple view matchers and actions like {@link ViewMatchers#withId},
@@ -26,16 +35,61 @@ public class MainActivityTest{
     @Rule
     public final ActivityTestRule<MainActivity> rule =
             new ActivityTestRule<>(MainActivity.class, true, false);
+    @Before
+    public void init(){
+        Context targetContext = InstrumentationRegistry.getInstrumentation()
+                .getTargetContext();
+        Intent intent = new Intent(targetContext, MainActivity.class);
+        intent.putExtra("studentid", "47657");
+        //intent.putExtra("screen", "3");
+        rule.launchActivity(intent);
+    }
+
     @Test
     public void someTest() {
         Context targetContext = InstrumentationRegistry.getInstrumentation()
                 .getTargetContext();
         Intent intent = new Intent(targetContext, MainActivity.class);
         intent.putExtra("studentid", "47657");
-        intent.putExtra("screen", "3");
+        //intent.putExtra("screen", "3");
         rule.launchActivity(intent);
 
+
         /* Your activity is initialized and ready to go. */
+    }
+
+    @Test
+    public void testSwitchWithId() {
+        onView(withId(R.id.tab_layout)).check(matches(not(isChecked())));
+        onView(withId(R.id.tab_layout)).perform(click());
+        onView(withId(R.id.tab_layout)).check(matches(isChecked()));
+
+    }
+
+    /**
+     * Click a switch using its label value.
+     * Espresso onView, withText, check, matches, isChecked, perform, not
+     */
+    @Test
+    public void testSwitchWithText() {
+        onView(withText("Message")).check(matches(not(isChecked())));
+        onView(withText("Absences")).perform(click());
+        onView(withText("Absences")).check(matches(isChecked()));
+    }
+
+    /**
+     * But why do that when you can chain it?
+     * A three line test can now be written in a single line.
+     * Espresso onView, withText, check, matches, not, isChecked, perform, click
+     */
+    @Test
+    public void testSwitchWithChainingExample() {
+        // Start with a ViewInteraction
+        onView(withText("Links"))
+                // Chain the methods you want to call.
+                .check(matches(not(isChecked())))
+                .perform(click())
+                .check(matches(isChecked()));
     }
 
 
